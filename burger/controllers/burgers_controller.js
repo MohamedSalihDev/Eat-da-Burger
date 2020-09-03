59055ca2d19af19f1b6e28ad5f2e460c7a949d15
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2020-09-03 14:56:51 -0700

    added code to require dependencies

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -13,7 +13,7 @@ router.get("/", function(req, res) {
   });
   
   router.post("/api/burgers", function(req, res) {
-    burger.create(["name"], [req.body.name], function(result) {
+    burger.create(["burger_name"], [req.body.name], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
 
 const PORT = process.env.PORT || 3000;
